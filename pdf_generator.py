760ab23b7c4214468b4b7b6edffaf213bd05152f
AdrianaCatrinoiu <124817643+AdrianaCatrinoiu@users.noreply.github.com>
2023-06-26 21:43:36 +0300

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 from fpdf import FPDF
-from database import create_database, validate_customer_id,generate_invoice, DATABASE_FILE
+from database import DATABASE_FILE
 
 def validate_customer_id(customer_id):
 
@@ -16,7 +16,6 @@ def validate_customer_id(customer_id):
     if count == 0:
         raise ValueError("Invalid customer ID.")
     
-
 
 def generate_invoice(customer_id, month, year):
 
@@ -41,9 +40,6 @@ def generate_invoice(customer_id, month, year):
 
     connexion.close()
 
-
-
-
     # Generate PDF invoice
     pdf = FPDF()
     pdf.add_page()
@@ -56,16 +52,15 @@ def generate_invoice(customer_id, month, year):
     pdf.cell(0, 10, txt=f"Customer Name: {customer[0]}", ln=True)
     pdf.cell(0, 10, txt=f"Address: {customer[1]}", ln=True)
     pdf.cell(0, 10, txt=f"Phone: {customer[2]}", ln=True, align="L")
-    pdf.cell(0, 10, txt=f"Date Generated Invoice: {consumption_details[4]}", ln=True)
+    
     pdf.ln(10)
 
     # Table
-
     pdf.set_font("Arial", "B", 12)
+    pdf.cell(30, 10, "Period", border=1)
     pdf.cell(30, 10, "Unit Price", border=1)
     pdf.cell(30, 10, "Index Initial", border=1)
     pdf.cell(30, 10, "Index End", border=1)
-    pdf.cell(30, 10, "Period", border=1)
     pdf.cell(30, 10, "Consum", border=1)
     pdf.cell(30, 10, "Total Amount", border=1)
     pdf.ln(10)
@@ -76,6 +71,9 @@ def generate_invoice(customer_id, month, year):
     pdf.cell(30, 10, str(consumption_details[1]), border=1)
     pdf.cell(30, 10, str(consumption_details[2]), border=1)
     pdf.cell(30, 10, str(consumption_details[2]*consumption_details[3]), border=1)
+    
+    pdf.ln(50)
+    pdf.cell(0, 10, txt=f"Date Generated Invoice: {consumption_details[4]}", ln=True)
 
     invoice_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "invoices")
     if not os.path.exists(invoice_dir):
@@ -87,3 +85,4 @@ def generate_invoice(customer_id, month, year):
     pdf.output(invoice_path)
 
     print(f"Invoice generated successfully: {invoice_path}")
+
