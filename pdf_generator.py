103b5d86967cc94a425feb6eba22d35e74a4cbf5
AdrianaCatrinoiu <124817643+AdrianaCatrinoiu@users.noreply.github.com>
2023-07-02 21:33:32 +0300

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -1,10 +1,13 @@
-import os
+from pathlib import Path
 import sqlite3
 from fpdf import FPDF
-from database import DATABASE_FILE
+from database import DATABASE_PATH
+
 
 def validate_customer_id(customer_id):
-    connexion = sqlite3.connect(DATABASE_FILE)
+    """Validates customer ID if it exists in the customers table."""
+
+    connexion = sqlite3.connect(str(DATABASE_PATH))
     c = connexion.cursor()
 
     c.execute("SELECT COUNT(*) FROM customers WHERE id=?", (customer_id,))
@@ -14,9 +17,12 @@ def validate_customer_id(customer_id):
 
     if count == 0:
         raise ValueError("Invalid customer ID.")
+    
 
 def generate_invoice(customer_id, month, year):
-    connexion = sqlite3.connect(DATABASE_FILE)
+    """Generate an invoice as a PDF file for a specific customer, month, and year."""
+
+    connexion = sqlite3.connect(str(DATABASE_PATH))
     c = connexion.cursor()
 
     # Get customer details
@@ -28,9 +34,7 @@ def generate_invoice(customer_id, month, year):
         raise ValueError("Invalid customer ID.")
 
     # Get consumption details
-    c.execute(
-        "SELECT index_initial, index_final, consumption, unit_price, date FROM bills WHERE customer_id=? AND month=? AND year=?",
-        (customer_id, month, year))
+    c.execute("SELECT index_initial, index_final, consumption, unit_price, date FROM bills WHERE customer_id=? AND month=? AND year=?", (customer_id, month, year))
     consumption_details = c.fetchone()
 
     if not consumption_details:
@@ -51,7 +55,6 @@ def generate_invoice(customer_id, month, year):
     pdf.cell(0, 10, txt=f"Customer Name: {customer[0]}", ln=True)
     pdf.cell(0, 10, txt=f"Address: {customer[1]}", ln=True)
     pdf.cell(0, 10, txt=f"Phone: {customer[2]}", ln=True, align="L")
-
     pdf.ln(10)
 
     # Table
@@ -64,23 +67,22 @@ def generate_invoice(customer_id, month, year):
     pdf.cell(30, 10, "Total Amount", border=1)
     pdf.ln(10)
     pdf.set_font("Arial", "", 12)
-    pdf.cell(30, 10, txt=f"{year}_{month}", border=1)
+    pdf.cell(30, 10,  txt=f"{year}_{month}", border=1)
     pdf.cell(30, 10, str(consumption_details[3]), border=1)
     pdf.cell(30, 10, str(consumption_details[0]), border=1)
     pdf.cell(30, 10, str(consumption_details[1]), border=1)
     pdf.cell(30, 10, str(consumption_details[2]), border=1)
-    pdf.cell(30, 10, str(consumption_details[2] * consumption_details[3]), border=1)
-
+    pdf.cell(30, 10, str(consumption_details[2]*consumption_details[3]), border=1)
+    
     pdf.ln(50)
     pdf.cell(0, 10, txt=f"Date Generated Invoice: {consumption_details[4]}", ln=True)
 
-    invoice_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "invoices")
-    if not os.path.exists(invoice_dir):
-        os.makedirs(invoice_dir)
-
+    
+    invoice_dir = Path(__file__).resolve().parent / "invoices"
+    invoice_dir.mkdir(exist_ok=True)
     invoice_filename = f"Invoice_{customer_id}_{month}_{year}.pdf"
-    invoice_path = os.path.join(invoice_dir, invoice_filename)
+    invoice_path = invoice_dir / invoice_filename
 
-    pdf.output(invoice_path)
+    pdf.output(str(invoice_path))
 
     print(f"Invoice generated successfully: {invoice_path}")
